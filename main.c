b3fcd09d01511b56c4e4852d66d0ba33cadd4d8a
Ayşe Gül DAL <118614193+ayseguldal@users.noreply.github.com>
2023-04-07 11:23:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,44 @@
-//1220505011 AYŞE GÜL DAL 
-//DİZİYİ TOPLAMI EŞİT ALT KÜMEYE BÖLME 
+//1220505011 AYÅE GÃœL DAL 
+//DÄ°ZÄ°YÄ° TOPLAMI EÅÄ°T ALT KÃœMEYE BÃ–LME 
 
 #include <stdio.h>
 #include <math.h>
 
-   //ALT KÜME OLUŞTURMA FONKSİYONU 
-int altkumeToplam(int arr[], int n, int sum)     //n:dizinin boyutu, sum:elemanları toplamı.
+   //ALT KÃœME OLUÅTURMA FONKSÄ°YONU 
+int altkumeToplam(int arr[], int n, int sum)     //n:dizinin boyutu, sum:elemanlarÄ± toplamÄ±.
 {
-    int altkume[n+1][sum+1];       // Alt küme toplamlarını saklamak için bir bool dizisi oluşturulur.
+    int altkume[n+1][sum+1];       // Alt kÃ¼me toplamlarÄ±nÄ± saklamak iÃ§in bir bool dizisi oluÅŸturulur.
    int i,j;
-    for (i = 0; i <= n; i++)  //Alt kümenin birinci sütunu girilen diznin boyutu kadar 1 ile dolduruluyor.
+    for (i = 0; i <= n; i++)  //Alt kÃ¼menin birinci sÃ¼tunu girilen diznin boyutu kadar 1 ile dolduruluyor.
         altkume[i][0] = 1;            
         
-    for (i = 1; i <= sum; i++)     //Alt kümenin birinci satırı gönderilen toplam kadar 1 ile dolduruluyor. 
+    for (i = 1; i <= sum; i++)     //Alt kÃ¼menin birinci satÄ±rÄ± gÃ¶nderilen toplam kadar 1 ile dolduruluyor. 
         altkume[0][i] = 0;
    
    
-                   // Toplamı tam olarak i'ye eşitlemek için alt kümeler bulunur.
-    for (i = 1; i <= n; i++) {       //Oluşturulan alt kümenin elemanlarını dolaşır.
+                   // ToplamÄ± tam olarak i'ye eÅŸitlemek iÃ§in alt kÃ¼meler bulunur.
+    for (i = 1; i <= n; i++) {       //OluÅŸturulan alt kÃ¼menin elemanlarÄ±nÄ± dolaÅŸÄ±r.
         for (j = 1; j <= sum; j++) {
-            if (j < arr[i-1])            //Koşul sağlanıyorsa aşağıdaki işlemleri uygular.
-                altkume[i][j] = altkume[i-1][j];         // Bu eleman alt kümeye dahil edilebilir mi?
-            if (j >= arr[i-1])           //Koşul sağlanıyorsa aşağıdaki işlemleri uygular.
-              altkume[i][j] = altkume[i-1][j] || altkume[i - 1][j-arr[i-1]];   //Altkümelerden en az birisinin 
+            if (j < arr[i-1])            //KoÅŸul saÄŸlanÄ±yorsa aÅŸaÄŸÄ±daki iÅŸlemleri uygular.
+                altkume[i][j] = altkume[i-1][j];         // Bu eleman alt kÃ¼meye dahil edilebilir mi?
+            if (j >= arr[i-1])           //KoÅŸul saÄŸlanÄ±yorsa aÅŸaÄŸÄ±daki iÅŸlemleri uygular.
+              altkume[i][j] = altkume[i-1][j] || altkume[i - 1][j-arr[i-1]];   //AltkÃ¼melerden en az birisinin saÄŸlamasÄ± yeterlidir.
         }
     }
-            // "toplam" değerine eşit iki alt küme var mı?
-    return altkume[n][sum];       //Oluşan alt kümeyi döndürür.
+            // "toplam" deÄŸerine eÅŸit iki alt kÃ¼me var mÄ±?
+    return altkume[n][sum];       //OluÅŸan alt kÃ¼meyi dÃ¶ndÃ¼rÃ¼r.
 }
 
 int main()
 {
-    int n;   //Dizinin eleman sayısını tanımladık.
+    int n;   //Dizinin eleman sayÄ±sÄ±nÄ± tanÄ±mladÄ±k.
     int i;
-    printf("Dizi eleman sayisini girin: ");   //Ekrana yazdırma.
-    scanf("%d", &n);  //Dizinin eleman sayısını kullanıcıdan aldık.
-    int set[n];   //set adında dizi tanımladık ve elemanlarımızı oraya atadık.
-    int toplam = 0;   //Dizinin elemanlarının toplamını atayacağımız bir değişken atadık.
+    printf("Dizi eleman sayisini girin: ");   //Ekrana yazdÄ±rma.
+    scanf("%d", &n);  //Dizinin eleman sayÄ±sÄ±nÄ± kullanÄ±cÄ±dan aldÄ±k.
+    int set[n];   //set adÄ±nda dizi tanÄ±mladÄ±k ve elemanlarÄ±mÄ±zÄ± oraya atadÄ±k.
+    int toplam = 0;   //Dizinin elemanlarÄ±nÄ±n toplamÄ±nÄ± atayacaÄŸÄ±mÄ±z bir deÄŸiÅŸken atadÄ±k.
     printf("Dizi elemanlarini girin:\n");
-    for (i = 0; i < n; i++) {   //Girilen elemanların toplamını bulan döngü.
+    for (i = 0; i < n; i++) {   //Girilen elemanlarÄ±n toplamÄ±nÄ± bulan dÃ¶ngÃ¼.
         scanf("%d", &set[i]);
         if(set[i]<0){
         	printf("NEGATIF BIR SAYI GIRILEMEZ");
@@ -47,9 +47,9 @@ int main()
         toplam += set[i];
     }
     
-    if (toplam % 2 == 0 && altkumeToplam(set, n, toplam/2))   //Toplamları eşit olacağı için ikiye bölünmeli ve altkumeToplam fonksiyonu sağlanmalı.
+    if (toplam % 2 == 0 && altkumeToplam(set, n, toplam/2))   //ToplamlarÄ± eÅŸit olacaÄŸÄ± iÃ§in ikiye bÃ¶lÃ¼nmeli ve altkumeToplam fonksiyonu saÄŸlanmalÄ±.
         printf("DIZI IKI ALT KUMESINE BOLUNEBILIR\n");
-    else                                                      //Yukarıdaki koşul sağlanmıyorsa bölünemez.
+    else                                                      //YukarÄ±daki koÅŸul saÄŸlanmÄ±yorsa bÃ¶lÃ¼nemez.
         printf("DIZI IKI ALT KUMESINE BOLUNEMEZ\n");
     return 0;
 }
